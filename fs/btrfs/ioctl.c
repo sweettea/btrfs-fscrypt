49fda202079ea511acbc39796054f46ab94928d1
David Sterba <dsterba@suse.cz>
2014-02-05 01:55:11 +0100

    wip
    Signed-off-by: David Sterba <dsterba@suse.cz>

--- fs/btrfs/ioctl.c
+++ fs/btrfs/ioctl.c
@@ -2319,7 +2319,7 @@ out_up_write:
 out_unlock:
 	mutex_unlock(&inode->i_mutex);
 	if (!err) {
-		shrink_dcache_sb(root->fs_info->sb);
+		shrink_dcache_parent(dentry);
 		btrfs_invalidate_inodes(dest);
 		d_delete(dentry);
 
