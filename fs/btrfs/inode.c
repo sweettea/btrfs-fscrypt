46178c7782067fcac0d6a08334441fd8d5322352
David Sterba <dsterba@suse.com>
2018-07-17 23:40:24 +0200

    compress_file_range sink arguments
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -433,11 +433,12 @@ static inline void inode_should_defrag(struct btrfs_inode *inode,
  * are written in the same order that the flusher thread sent them
  * down.
  */
-static noinline int compress_file_range(struct inode *inode,
-					struct page *locked_page,
-					u64 start, u64 end,
-					struct async_cow *async_cow)
+static noinline int compress_file_range(struct async_cow *async_cow)
 {
+	struct inode *inode = async_cow->inode;
+	struct page *locked_page = async_cow->locked_page;
+	u64 start = async_cow->start;
+	u64 end = async_cow->end;
 	struct btrfs_fs_info *fs_info = btrfs_sb(inode->i_sb);
 	u64 blocksize = fs_info->sectorsize;
 	u64 actual_end;
@@ -1129,8 +1130,7 @@ static noinline void async_cow_start(struct btrfs_work *work)
 	int num_added;
 	async_cow = container_of(work, struct async_cow, work);
 
-	num_added = compress_file_range(async_cow->inode, async_cow->locked_page,
-			    async_cow->start, async_cow->end, async_cow);
+	num_added = compress_file_range(async_cow);
 	if (num_added == 0) {
 		btrfs_add_delayed_iput(async_cow->inode);
 		async_cow->inode = NULL;
