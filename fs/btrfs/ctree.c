212f7564065bdfda0a55ab1ef52a4868572359de
David Sterba <dsterba@suse.com>
2020-05-26 17:12:50 +0200

    btrfs: add authenticated plus secondary checksum for sha256 and blake2b
    Add two new checksums that implemented authenticated checksum with an
    additional plain hash.
    
    They can be selected as 'authsum-sha256' or 'authsum-blake2b', where the
    secondary hash is always xxhash64.
    
    The point is to provide basic verification of metadata and data blocks
    when the authentication key is not available.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/ctree.c
+++ fs/btrfs/ctree.c
@@ -46,6 +46,14 @@ static const struct btrfs_csums {
 	[BTRFS_CSUM_TYPE_AUTH_BLAKE2] = { .size = 32, .name = "auth-blake2b",
 					  .driver = "blake2b-256",
 					  .flags = BTRFS_CSUM_TYPE_AUTH },
+	[BTRFS_CSUM_TYPE_AUTHSUM_SHA256] = { .size = 32, .name = "authsum-sha256",
+					  .driver = "hmac(sha256)",
+					  .flags = BTRFS_CSUM_TYPE_AUTH |
+						   BTRFS_CSUM_TYPE_AUTHSUM },
+	[BTRFS_CSUM_TYPE_AUTHSUM_BLAKE2] = { .size = 32, .name = "authsum-blake2b",
+					  .driver = "blake2b-256",
+					  .flags = BTRFS_CSUM_TYPE_AUTH |
+						   BTRFS_CSUM_TYPE_AUTHSUM },
 };
 
 int btrfs_super_csum_size(const struct btrfs_super_block *s)
