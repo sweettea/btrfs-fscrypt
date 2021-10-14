7188b855c857d8c28f02c1c38255e1905b4d9084
David Sterba <dsterba@suse.com>
2021-10-15 00:46:36 +0200

    btrfs: update RAID1 and RAID10 calculations in statfs
    The RAID1* profiles don't need to be enumerated, we can use the mask.
    
    For RAID10 we need to take into account the degenerate mode, the
    redundancy level is also 'ncopies' and that like RAID0, it spans all the
    devices. There's clearly a maximum of nr_devices that the allocator can
    take into account. Using 4 here means the non-degenerate minimum number
    of devices (later adjusted to the nr_devices if smaller), so the
    redundancy is not important here, we always want to simulate using the
    full stripe, so it's nr_devices for RAID10.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -2145,14 +2145,10 @@ static inline int btrfs_calc_avail_data_space(struct btrfs_fs_info *fs_info,
 
 	if (type & BTRFS_BLOCK_GROUP_RAID0)
 		num_stripes = nr_devices;
-	else if (type & BTRFS_BLOCK_GROUP_RAID1)
-		num_stripes = 2;
-	else if (type & BTRFS_BLOCK_GROUP_RAID1C3)
-		num_stripes = 3;
-	else if (type & BTRFS_BLOCK_GROUP_RAID1C4)
-		num_stripes = 4;
+	else if (type & BTRFS_BLOCK_GROUP_RAID1_MASK)
+		num_stripes = rattr->ncopies;
 	else if (type & BTRFS_BLOCK_GROUP_RAID10)
-		num_stripes = 4;
+		num_stripes = nr_devices;
 
 	/* Adjust for more than 1 stripe per device */
 	min_stripe_size = rattr->dev_stripes * BTRFS_STRIPE_LEN;
