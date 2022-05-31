8f877d65a661708ec56727dabb940644b21809e8
Omar Sandoval <osandov@osandov.com>
2022-05-31 17:39:24 -0400

    btrfs: permit searching for nokey names for removal
    Deleting an encrypted file must always be permitted, even if the user
    does not have the appropriate key. Therefore, for listing an encrypted
    directory, so-called 'nokey' names are provided, and these nokey names
    must be sufficient to look up and delete the appropriate encrypted
    files. See 'struct fscrypt_nokey_name' for more information on the
    format of these names.
    
    The first part of supporting nokey names is allowing lookups by nokey
    name. Only a few entry points need to support these: deleting a
    directory, file, or subvolume -- each of these call
    fscrypt_setup_filename() with a '1' argument, indicating that the key is
    not required and therefore a nokey name may be provided. If a nokey name
    is provided, the fscrypt_name returned by fscrypt_setup_filename() will
    not have its disk_name field populated, but will have various other
    fields set.
    
    This change alters the relevant codepaths to pass a complete
    fscrypt_name anywhere that it might contain a nokey name. When it does
    contain a nokey name, the first time the name is successfully matched to
    a stored name populates the disk name field of the fscrypt_name,
    allowing the caller to use the normal disk name codepaths afterward.
    Otherwise, the matching functionality is in close analogue to the
    function fscrypt_match_name().
    
    Functions where most callers are providing a fscrypt_str are duplicated
    and adapted for a fscrypt_name, and functions where most callers are
    providing a fscrypt_name are changed to so require at all callsites.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>
    Signed-off-by: Sweet Tea Dorminy <sweettea-kernel@dorminy.me>

--- fs/btrfs/fscrypt.h
+++ fs/btrfs/fscrypt.h
@@ -4,6 +4,7 @@
 #define BTRFS_FSCRYPT_H
 
 #include <linux/fscrypt.h>
+#include "extent_io.h"
 
 #define BTRFS_ENCRYPTION_POLICY_BITS 2
 #define BTRFS_ENCRYPTION_CTXSIZE_BITS 6
@@ -23,6 +24,24 @@ struct btrfs_fscrypt_extent_context {
 	u8 len;
 };
 #endif
+
+#ifdef CONFIG_FS_ENCRYPTION
+bool btrfs_fscrypt_match_name(struct fscrypt_name *fname,
+			      struct extent_buffer *leaf,
+			      unsigned long de_name, u32 de_name_len);
+
+#else
+static inline bool btrfs_fscrypt_match_name(struct fscrypt_name *fname,
+					    struct extent_buffer *leaf,
+					    unsigned long de_name,
+					    u32 de_name_len)
+{
+	if (de_name_len != fname_len(fname))
+		return false;
+	return !memcmp_extent_buffer(leaf, fname->disk_name.name, de_name,
+				     de_name_len);
+}
+#endif /* CONFIG_FS_ENCRYPTION */
 
 static inline void btrfs_unpack_encryption(u8 encryption,
 					   u8 *policy,
