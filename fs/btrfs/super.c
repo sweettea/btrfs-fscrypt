db3bdeaedf9084f2634a14eff234522bb023d5fa
Omar Sandoval <osandov@osandov.com>
2022-05-31 18:27:40 -0400

    btrfs: Add new FEATURE_INCOMPAT_ENCRYPT feature flag.
    As encrypted files will be incompatible with older filesystem versions,
    new filesystems should be created with an incompat flag for fscrypt,
    which will gate access to the encryption ioctls.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>
    Signed-off-by: Sweet Tea Dorminy <sweettea-kernel@dorminy.me>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -2412,6 +2412,11 @@ static int __init btrfs_print_mod_info(void)
 #else
 			", fsverity=no"
 #endif
+#ifdef CONFIG_FS_ENCRYPTION
+			", fscrypt=yes"
+#else
+			", fscrypt=no"
+#endif
 			;
 	pr_info("Btrfs loaded, crc32c=%s%s\n", crc32c_impl(), options);
 	return 0;
