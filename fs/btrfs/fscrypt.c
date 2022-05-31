35cb4e6081929f4a1ff80f4c2bcec397b45e4baa
Omar Sandoval <osandov@osandov.com>
2022-05-31 17:39:24 -0400

    btrfs: factor a fscrypt_name matching method
    Now that everything in btrfs is dealing in fscrypt_names, fscrypt has a
    useful function, fscrypt_match_name(), to check whether a fscrypt_name
    matches a provided buffer. However, btrfs buffers are struct
    extent_buffer rather than a raw char array, so we need to implement our
    own imitation of fscrypt_match_name() that deals in extent_buffers,
    falling back to a simple memcpy if fscrypt isn't compiled. We
    can then use this matching method in btrfs_match_dir_item_name() and
    other locations.
    
    This also provides a useful occasion to introduce the new fscrypt file
    for btrfs, handling the fscrypt-specific functions needed.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>
    Signed-off-by: Sweet Tea Dorminy <sweettea-kernel@dorminy.me>

---
+++ fs/btrfs/fscrypt.c
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Copyright (C) 2020 Facebook
+ */
+
+#include "ctree.h"
+#include "fscrypt.h"
+
+/* fscrypt_match_name() but for an extent_buffer. */
+bool btrfs_fscrypt_match_name(const struct fscrypt_name *fname,
+			      struct extent_buffer *leaf, unsigned long de_name,
+			      u32 de_name_len)
+{
+	const struct fscrypt_nokey_name *nokey_name =
+		(const void *)fname->crypto_buf.name;
+	u8 digest[SHA256_DIGEST_SIZE];
+
+	if (likely(fname->disk_name.name)) {
+		if (de_name_len != fname->disk_name.len)
+			return false;
+		return !memcmp_extent_buffer(leaf, fname->disk_name.name,
+					     de_name, de_name_len);
+	}
+	if (de_name_len <= sizeof(nokey_name->bytes))
+		return false;
+	if (memcmp_extent_buffer(leaf, nokey_name->bytes, de_name,
+				 sizeof(nokey_name->bytes)))
+		return false;
+	extent_buffer_sha256(leaf, de_name + sizeof(nokey_name->bytes),
+			     de_name_len - sizeof(nokey_name->bytes), digest);
+	return !memcmp(digest, nokey_name->sha256, sizeof(digest));
+}
