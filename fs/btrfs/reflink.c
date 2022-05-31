6b95042a1681fad781252bb6837b7552f0cc2cb5
Omar Sandoval <osandov@osandov.com>
2022-05-31 17:51:52 -0400

    btrfs: disable various operations on encrypted inodes
    Initially, only normal data extents, using the normal (non-direct) IO
    path, will be encrypted. This change forbids various other bits:
    - allows reflinking only if both inodes have the same encryption status
    - disables compressing encrypted inodes
    - disables direct IO on encrypted inodes
    - disable inline data on encrypted inodes
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>
    Signed-off-by: Sweet Tea Dorminy <sweettea-kernel@dorminy.me>

--- fs/btrfs/reflink.c
+++ fs/btrfs/reflink.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include <linux/blkdev.h>
+#include <linux/fscrypt.h>
 #include <linux/iversion.h>
 #include "ctree.h"
 #include "fs.h"
@@ -810,6 +811,12 @@ static int btrfs_remap_file_range_prep(struct file *file_in, loff_t pos_in,
 
 		ASSERT(inode_in->i_sb == inode_out->i_sb);
 	}
+
+	/*
+	 * Can only reflink encrypted files if both files are encrypted.
+	 */
+	if (IS_ENCRYPTED(inode_in) != IS_ENCRYPTED(inode_out))
+		return -EINVAL;
 
 	/* Don't make the dst file partly checksummed */
 	if ((BTRFS_I(inode_in)->flags & BTRFS_INODE_NODATASUM) !=
