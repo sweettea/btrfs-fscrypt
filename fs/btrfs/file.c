fee598e31e7ab357937cfbbfcd21234749af33f8
Omar Sandoval <osandov@osandov.com>
2022-06-23 13:21:37 -0400

    btrfs: start using fscrypt hooks
    In order to appropriately encrypt, create, open, rename, and various
    symlink operations must call fscrypt hooks. These determine whether the
    inode should be encrypted and do other preparatory actions. The
    superblock must have fscrypt operations registered, so implement the
    minimal set also, and introduce the new fscrypt.[ch] files to hold the
    fscrypt-specific functionality. Also add the key prefix for fscrypt v1
    keys.
    
    Signed-off-by: Omar Sandoval <osandov@osandov.com>
    Signed-off-by: Sweet Tea Dorminy <sweettea-kernel@dorminy.me>

--- fs/btrfs/file.c
+++ fs/btrfs/file.c
@@ -3702,6 +3702,9 @@ static int btrfs_file_open(struct inode *inode, struct file *filp)
 
 	filp->f_mode |= FMODE_NOWAIT | FMODE_BUF_RASYNC | FMODE_BUF_WASYNC |
 		        FMODE_CAN_ODIRECT;
+	ret = fscrypt_file_open(inode, filp);
+	if (ret)
+		return ret;
 
 	ret = fsverity_file_open(inode, filp);
 	if (ret)
