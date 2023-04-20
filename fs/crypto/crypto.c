4a46aef4d7c3f26f13e545308d3d3c016a6e989d
Sweet Tea Dorminy <sweettea-kernel@dorminy.me>
2023-04-20 05:37:41 -0400

    fscrypt: adjust effective lblks based on extents
    If a filesystem uses extent-based encryption, then the offset within a
    file is not a constant which can be used for calculating an IV.
    For instance, the same extent could be blocks 0-8 in one file, and
    blocks 100-108 in another file. Instead, the block offset within the
    extent must be used instead.
    
    Update all uses of logical block offset within the file to use logical
    block offset within the extent, if applicable.
    
    Signed-off-by: Sweet Tea Dorminy <sweettea-kernel@dorminy.me>

--- fs/crypto/crypto.c
+++ fs/crypto/crypto.c
@@ -107,8 +107,9 @@ int fscrypt_crypt_block(const struct inode *inode, fscrypt_direction_t rw,
 	struct skcipher_request *req = NULL;
 	DECLARE_CRYPTO_WAIT(wait);
 	struct scatterlist dst, src;
+	u64 ci_offset = 0;
 	struct fscrypt_info *ci =
-		fscrypt_get_lblk_info(inode, lblk_num, NULL, NULL);
+		fscrypt_get_lblk_info(inode, lblk_num, &ci_offset, NULL);
 	struct crypto_skcipher *tfm = fscrypt_get_contents_tfm(ci);
 	int res = 0;
 
@@ -123,7 +124,7 @@ int fscrypt_crypt_block(const struct inode *inode, fscrypt_direction_t rw,
 		res = -EINVAL;
 		goto unlock;
 	}
-	fscrypt_generate_iv(&iv, lblk_num, ci);
+	fscrypt_generate_iv(&iv, ci_offset, ci);
 
 	req = skcipher_request_alloc(tfm, gfp_flags);
 	if (!req) {
