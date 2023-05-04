016d8d2dbe3a24d7a4fb1be337015960da4b8f9c
Sweet Tea Dorminy <sweettea-kernel@dorminy.me>
2023-05-04 06:21:25 -0400

    fscrypt: allow infos to be owned by extents
    In order to notify extents when their info is part of a master key which
    is going away, the fscrypt_info must have a backpointer to the extent
    somehow. Similarly, if a fscrypt_info is owned by an extent, the info
    must not have a pointer to an inode -- multiple inodes may reference a
    extent, and the first inode to cause an extent's creation may have a
    lifetime much shorter than the extent, so there is no inode pointer
    safe to track in an extent-owned info. Therefore, this adds a new
    pointer for extent-owned infos to track their extent and updates
    fscrypt_setup_encryption_info() accordingly.
    
    Since it's simple to track the piece of extent memory pointing to the
    info, and for the extent to then go from such a pointer to the whole
    extent via container_of(), we store that. Although some sort of generic
    void * or some artificial fscrypt_extent embedded structure would also
    work, those would require additional plumbing which doesn't seem
    strictly required or clarifying.
    
    Signed-off-by: Sweet Tea Dorminy <sweettea-kernel@dorminy.me>

--- fs/crypto/fscrypt_private.h
+++ fs/crypto/fscrypt_private.h
@@ -241,6 +241,12 @@ struct fscrypt_info {
 	/* Back-pointer to the inode */
 	struct inode *ci_inode;
 
+	/*
+	 * Back-pointer to the info pointer in the extent, for infos owned by
+	 * an extent.
+	 */
+	struct fscrypt_info **ci_info_ptr;
+
 	/* The superblock of the filesystem to which this info pertains */
 	struct super_block *ci_sb;
 
