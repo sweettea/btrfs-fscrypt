275319a4c0659e2fc8a905ca4e08405dde34560d
Paul E. McKenney <paulmck@kernel.org>
2024-03-27 15:21:31 -0700

    sh: Emulate one-byte and two-byte cmpxchg
    Use the new cmpxchg_emu_u8() and cmpxchg_emu_u16() to emulate one-byte
    and two-byte cmpxchg() on sh.
    
    Signed-off-by: Paul E. McKenney <paulmck@kernel.org>
    Cc: Andi Shyti <andi.shyti@linux.intel.com>
    Cc: Palmer Dabbelt <palmer@rivosinc.com>
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: <linux-sh@vger.kernel.org>

--- arch/sh/include/asm/cmpxchg.h
+++ arch/sh/include/asm/cmpxchg.h
@@ -56,6 +56,10 @@ static inline unsigned long __cmpxchg(volatile void * ptr, unsigned long old,
 		unsigned long new, int size)
 {
 	switch (size) {
+	case 1:
+		return cmpxchg_emu_u8((volatile u8 *)ptr, old, new);
+	case 2:
+		return cmpxchg_emu_u16((volatile u16 *)ptr, old, new);
 	case 4:
 		return __cmpxchg_u32(ptr, old, new);
 	}
